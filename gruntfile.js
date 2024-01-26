4fdaecea708438d0bdc0a81fef2988800eeccc18
Daniela Alves <dani.alvesm22@gmail.com>
2024-01-26 13:52:57 -0300

    :sparkles: finalização do exercício

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,24 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        },
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            }
+        }
     })
+
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+    grunt.loadNpmTasks('grunt-contrib-less')
+    
+    grunt.registerTask('default', ['uglify', 'less'])
 }
